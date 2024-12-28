ed62852a7219dd78bdc95421d708ade8db6fd480
AuTa <auta520@live.com>
2024-12-28 20:40:08 +0800

    ✨ add new color RegExp
    Add contrastColorResolver.

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,8 @@ export type ContrastOptions = {
     defaultDelta?: number
 }
 
+const defaultContrastOptions: ContrastOptions = { suffix: 'foreground', defaultDelta: 60 }
+
 /**
  * Split color object to nested object.
  *
@@ -107,15 +109,47 @@ const REMOVE_COMMENT_RE = /\/\*.*?\*\//g
  */
 const CONTRAST_VAR = '--contrast-dark'
 
-function contrastColor(match: RegExpMatchArray, css: CSSObject, defaultDelta = 60) {
+/**
+ * Adjusts the color contrast of a given CSS object based on a specified delta and sign.
+ *
+ * This function modifies the `color` property of the provided CSS object by applying
+ * a contrast adjustment using the OKLCH color model. It calculates the new color by
+ * reducing the lightness (l) according to the contrast variable and the specified delta.
+ *
+ * @see https://developer.chrome.com/blog/css-relative-color-syntax#contrast_a_color
+ * @param css - The CSS object containing the `color` property to be adjusted.
+ * @param delta - The contrast difference threshold (0-100), defaults to 60.
+ * @param sign - The sign used in the contrast calculation, defaults to an empty string.
+ * @returns The modified CSS object with the adjusted color.
+ */
+function contrastColor(css: CSSObject, delta = 60, sign: '' | '+' | '-' = '') {
     let color = css.color
     if (!color || typeof color !== 'string') return
     color = color.replace(REMOVE_COMMENT_RE, '')
-    const delta = match.groups?.delta ?? defaultDelta
-    const sign = match.groups?.sign ?? ''
     const oklch = `oklch(from ${color} calc(l - var(${CONTRAST_VAR}) * ${sign}.${delta}) c h)`
     css.color = oklch
     return css
+}
+
+/**
+ * Returns a resolver function that resolves a CSS object with a contrast adjusted color based on a specified delta and sign.
+ *
+ * The resolver function takes a regular expression match and a RuleContext object as arguments.
+ * It resolves the color from the match using the colorResolver function and then adjusts the color
+ * using the contrastColor function with the specified default delta and the sign from the match.
+ * If the color cannot be resolved, the resolver returns undefined.
+ *
+ * @param defaultDelta - The default contrast difference threshold (0-100), defaults to 60.
+ * @returns A resolver function that takes a regular expression match and a RuleContext object as arguments.
+ */
+function contrastColorResolver(defaultDelta?: number) {
+    return (match: RegExpMatchArray, ctx: RuleContext) => {
+        const color = colorResolver('color', 'text', 'textColor')(match, ctx) as CSSObject
+        if (!color) return
+        const delta = (match.groups?.delta ?? defaultDelta) as number
+        const sign = match.groups?.sign as '' | '+' | '-'
+        return contrastColor(color, delta, sign)
+    }
 }
 
 export const presetColor = (options?: PresetColorOptions) => {
@@ -123,19 +157,18 @@ export const presetColor = (options?: PresetColorOptions) => {
     if (options?.colors) {
         colors = splitColors(options.colors)
     }
+
     const rules: Rule<Theme>[] = []
     const preflights = []
     if (options?.contrast) {
-        const suffix = options.contrast === true ? 'foreground' : (options.contrast.suffix ?? 'foreground')
-        const delta = options.contrast !== true ? options.contrast.defaultDelta : undefined
-        rules.push([
-            new RegExp(`^text-(.*)-${suffix}(?:-(?<sign>\\+|-)?(?<delta>\\d+))?$`),
-            (match: RegExpMatchArray, ctx: RuleContext<Theme>) => {
-                const color = colorResolver('color', 'text', 'textColor')(match, ctx) as CSSObject
-                if (!color) return
-                return contrastColor(match, color, delta)
-            },
-        ])
+        const contrast = options.contrast === true ? defaultContrastOptions : options.contrast
+        const suffix = contrast.suffix ?? defaultContrastOptions.suffix
+        const defaultDelta = contrast.defaultDelta ?? defaultContrastOptions.defaultDelta
+        const regBase = `-${suffix}(?:-(?<sign>\\+|-)?(?<delta>\\d+))?$`
+        rules.push(
+            [new RegExp(`^text-(?:color-)?(.+)${regBase}`), contrastColorResolver(defaultDelta)],
+            [new RegExp(`^(?:color|c)-?(.+)${regBase}`), contrastColorResolver(defaultDelta)],
+        )
         preflights.push({
             getCSS: () => `
                 :root {
