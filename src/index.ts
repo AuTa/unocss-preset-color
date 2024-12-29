5ed8f623d77f4b0a5bfecf8f87ddf5ecf56cd491
AuTa <auta520@live.com>
2024-12-29 11:20:59 +0800

    ✨ contrast color can fallback
    对比色可以回落到 lch、原本前景色。

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import type { Colors, Theme } from '@unocss/preset-mini/theme'
-import { colorResolver } from '@unocss/preset-mini/utils'
-import type { CSSObject, Rule, RuleContext } from 'unocss'
+import type { Rule } from 'unocss'
+
+import { CONTRAST_VAR, contrastColorResolver } from './contrast-color'
 
 /**
  * Preset options
@@ -101,57 +102,6 @@ function splitColors(colors: Colors): Colors {
     return colors
 }
 
-const REMOVE_COMMENT_RE = /\/\*.*?\*\//g
-
-/**
- * CSS variable name for dark contrast adjustments
- * The variable is used to define the contrast level in dark mode themes
- */
-const CONTRAST_VAR = '--contrast-dark'
-
-/**
- * Adjusts the color contrast of a given CSS object based on a specified delta and sign.
- *
- * This function modifies the `color` property of the provided CSS object by applying
- * a contrast adjustment using the OKLCH color model. It calculates the new color by
- * reducing the lightness (l) according to the contrast variable and the specified delta.
- *
- * @see https://developer.chrome.com/blog/css-relative-color-syntax#contrast_a_color
- * @param css - The CSS object containing the `color` property to be adjusted.
- * @param delta - The contrast difference threshold (0-100), defaults to 60.
- * @param sign - The sign used in the contrast calculation, defaults to an empty string.
- * @returns The modified CSS object with the adjusted color.
- */
-function contrastColor(css: CSSObject, delta = 60, sign: '' | '+' | '-' = '') {
-    let color = css.color
-    if (!color || typeof color !== 'string') return
-    color = color.replace(REMOVE_COMMENT_RE, '')
-    const oklch = `oklch(from ${color} calc(l - var(${CONTRAST_VAR}) * ${sign}.${delta}) c h)`
-    css.color = oklch
-    return css
-}
-
-/**
- * Returns a resolver function that resolves a CSS object with a contrast adjusted color based on a specified delta and sign.
- *
- * The resolver function takes a regular expression match and a RuleContext object as arguments.
- * It resolves the color from the match using the colorResolver function and then adjusts the color
- * using the contrastColor function with the specified default delta and the sign from the match.
- * If the color cannot be resolved, the resolver returns undefined.
- *
- * @param defaultDelta - The default contrast difference threshold (0-100), defaults to 60.
- * @returns A resolver function that takes a regular expression match and a RuleContext object as arguments.
- */
-function contrastColorResolver(defaultDelta?: number) {
-    return (match: RegExpMatchArray, ctx: RuleContext) => {
-        const color = colorResolver('color', 'text', 'textColor')(match, ctx) as CSSObject
-        if (!color) return
-        const delta = (match.groups?.delta ?? defaultDelta) as number
-        const sign = match.groups?.sign as '' | '+' | '-'
-        return contrastColor(color, delta, sign)
-    }
-}
-
 export const presetColor = (options?: PresetColorOptions) => {
     let colors: Colors = {}
     if (options?.colors) {
@@ -164,10 +114,10 @@ export const presetColor = (options?: PresetColorOptions) => {
         const contrast = options.contrast === true ? defaultContrastOptions : options.contrast
         const suffix = contrast.suffix ?? defaultContrastOptions.suffix
         const defaultDelta = contrast.defaultDelta ?? defaultContrastOptions.defaultDelta
-        const regBase = `-${suffix}(?:-(?<sign>\\+|-)?(?<delta>\\d+))?$`
+        const regBase = `(?<bg>.+)-(?<suffix>${suffix})(?:-(?<sign>\\+|-)?(?<delta>\\d+))?$`
         rules.push(
-            [new RegExp(`^text-(?:color-)?(.+)${regBase}`), contrastColorResolver(defaultDelta)],
-            [new RegExp(`^(?:color|c)-?(.+)${regBase}`), contrastColorResolver(defaultDelta)],
+            [new RegExp(`^text-(?:color-)?${regBase}`), contrastColorResolver(defaultDelta)],
+            [new RegExp(`^(?:color|c)-?${regBase}`), contrastColorResolver(defaultDelta)],
         )
         preflights.push({
             getCSS: () => `
